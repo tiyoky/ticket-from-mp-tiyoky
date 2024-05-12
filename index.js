a18eec856c76896193aec196840930cf3ae8dde1
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-05-12 18:06:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const salonId = "process.env.salonid";
+const salonId = process.env.salonid;
 
 client.once('ready', () => {
     console.log('Connecté en tant que ' + client.user.tag);
     client.user.setActivity('ticket par dm - by tiyoky', { type: 'WATCHING' });
 });
 
-
 client.on('message', async message => {
     if (message.channel.type === 'dm') { 
         const salon = client.channels.cache.get(salonId); 
-        if (salon) { 
-            salon.send(`Message de ${message.author.tag}: ${message.content}`); 
+        if (salon) {
+            const embed = new Discord.MessageEmbed()
+                .setColor('#00ff00')
+                .setTitle(`Message de ${message.author.username}`)
+                .setDescription(message.content);
+            salon.send(embed); 
         }
     }
 });
 
+client.login(process.env.TOKEN);
 
-client.login('process.env.TOKEN');
