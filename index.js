655ab0fb3f57e5349430c78208a9daa1923311a9
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-05-12 18:29:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ client.on('messageCreate', async message => {
     if (message.channel.type === 'DM') {
         const guilds = client.guilds.cache.array();
         for (const guild of guilds) {
-            const channel = guild.channels.cache.get('1239338901718433814');
-            if (channel && channel.type === 'GUILD_TEXT') {
+            const channels = guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT' && channel.name.toLowerCase().includes('dm_test'));
+            for (const [, channel] of channels) {
                 const embed = new Discord.MessageEmbed()
-                    .setColor('#00ff00') 
+                    .setColor('#00ff00') // Vert
                     .setTitle(`Message de ${message.author.username}`)
                     .setDescription(message.content);
                 await channel.send({ embeds: [embed] });
-                break; 
             }
         }
     }
 });
+
 
 
 
