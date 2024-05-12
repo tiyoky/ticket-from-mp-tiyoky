52651d6c22e1f31ae88bea1008e54772fb13b8ed
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-05-12 18:26:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const client = new Discord.Client({
         Discord.Intents.FLAGS.DIRECT_MESSAGES
     ] 
 });
-const salonId = process.env.salonid;
 
 client.once('ready', () => {
     console.log('Connecté en tant que ' + client.user.tag);
     client.user.setActivity('ticket par dm - by tiyoky', { type: 'WATCHING' });
 });
 
-client.on('message', async message => {
-    if (message.channel.type === 'DM') { 
-        const salon = client.channels.cache.get(salonId); 
-        if (salon) {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#00ff00')
-                .setTitle(`Message de ${message.author.username}`)
-                .setDescription(message.content);
-            salon.send(embed); 
+client.on('messageCreate', async message => {
+    if (message.channel.type === 'DM') {
+        const guilds = client.guilds.cache.array();
+        for (const guild of guilds) {
+            const channel = guild.channels.cache.get('1239338901718433814');
+            if (channel && channel.type === 'GUILD_TEXT') {
+                const embed = new Discord.MessageEmbed()
+                    .setColor('#00ff00') 
+                    .setTitle(`Message de ${message.author.username}`)
+                    .setDescription(message.content);
+                await channel.send({ embeds: [embed] });
+                break; 
+            }
         }
     }
 });
+
+
 
 client.login(process.env.TOKEN);
 
